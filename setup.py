fb2e836134be71ca6fb9b962fb72814b837a3c2f
Pol <psanlorenzo@vinissimus.com>
2018-10-05 09:11:44 +0200

    correcció i millores

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'newsletter_tester.py',
-    version = '1.0.0',
-    author = 'Jordi Collell',
-    author_email = 'jordic@vinissimus.com',
-    description = 'Enviar emails renderitzats en html',
-    long_description = open('README.md').read(),
+    name='mail_tester.py',
+    version='1.0.0',
+    author='Jordi Collell',
+    author_email='jordic@vinissimus.com',
+    description='Enviar emails renderitzats en html',
+    long_description=open('README.md').read(),
     install_requires=[
         'aiohttp',
         'aiohttp_jinja2',
         'asyncio',
         'jinja2',
     ],
-    url = 'https://github.com/vinissimus/mail_tester',
-    packages = find_packages(),
-    classifiers = [
+    url='https://github.com/vinissimus/mail_tester',
+    packages=find_packages(),
+    classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6',
     ]
